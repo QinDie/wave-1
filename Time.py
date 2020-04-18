16f03cc2e8a5097d76142b55430518f24f7a5675
QinDie <62633170+QinDie@users.noreply.github.com>
2020-04-17 23:15:10 -0400

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,6 @@
+days =int(input("days="))
+hours = int(input("hours="))
+minutes = int(input("minutes="))
+seconds = int(input("seconds="))
+total =days * 60*60*24 + hours * 60*60 + minutes *60+seconds 
+print(total , "seconds")
