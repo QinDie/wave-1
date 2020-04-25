00dbb7ba0c9d257950c505db326e0cae8436df88
QinDie <62633170+QinDie@users.noreply.github.com>
2020-04-24 20:33:48 -0400

    Add files via upload

---
+++ Colors of squares .py
@@ -0,0 +1,19 @@
+one = ["1","3","5","7"]
+two = ["2","4","6","8"]
+three = ["a","c","e","g"]
+four = ["b","d","f","h"]
+
+pos = input()
+num = pos[1]
+let = pos[0]
+if let in three:
+    if num in one:
+         print("black")
+    else :
+         print("white")
+elif let in four :
+    if num in one :
+         print("black")
+    else:
+         print("white")
+    
